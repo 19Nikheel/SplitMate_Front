807ab2fd84f28a3caff45ae10227a87815b45859
Nikheel Kumar <flash.nik19@gmail.com>
2025-08-21 11:49:50 +0530

    got48

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -36,7 +36,7 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const checkAuth = async () => {
-      if (!arr.includes(location.hash.replace("/^#/", ""))) {
+      if (!arr.includes(location.hash.replace(/^#/, ""))) {
         if (!accessToken) {
           navigate("/login");
           setLoading(false);
@@ -103,7 +103,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     checkAuth();
-  }, [navigate, location.hash.replace("/^#/", "")]);
+  }, [navigate, location.hash.replace(/^#/, "")]);
 
   const login = async (userName, password) => {
     try {
